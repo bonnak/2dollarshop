2587a35388dbd75ae39759a065d89880495a6a6c
Hor <sokhor.gr@gmail.com>
2020-08-29 22:10:42 +1000

    Created LastDeal page

--- components/LatestDeals.js
+++ components/LatestDeals.js
@@ -1,5 +1,24 @@
+import { Card, ListGroup, ListGroupItem } from 'react-bootstrap';
+
 export default function LatestDeals() {
-  return <div>
-    Latest deals
+  return <div className="mt-2">
+    <Card border="light">
+      <Card.Header as="h5">TODAY</Card.Header>
+      <Card.Body>
+        <Card.Title>Light Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the bulk
+          of the card's content.
+        </Card.Text>
+      </Card.Body> <hr />
+      <Card.Body>
+        <Card.Title>Light Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the bulk
+          of the card's content.
+        </Card.Text>
+      </Card.Body> <hr />
+
+    </Card>
   </div>
 }
