50406016bd6deb240545b66fa8c9aed9639c9c07
Hor <sokhor.gr@gmail.com>
2020-09-05 20:02:22 +1000

    Latest deal

--- components/LatestDeals.js
+++ components/LatestDeals.js
@@ -1,24 +1,39 @@
-import { Card, ListGroup, ListGroupItem } from 'react-bootstrap';
+import { Card } from 'react-bootstrap';
+import Avatar from './Avatar';
 
-export default function LatestDeals() {
-  return <div className="mt-2">
-    <Card border="light">
-      <Card.Header as="h5">TODAY</Card.Header>
-      <Card.Body>
-        <Card.Title>Light Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the bulk
-          of the card's content.
-        </Card.Text>
-      </Card.Body> <hr />
-      <Card.Body>
-        <Card.Title>Light Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the bulk
-          of the card's content.
-        </Card.Text>
-      </Card.Body> <hr />
+function Deal ({ title, description, image, createdAt, expiredAt, username, avatar, tags }) {
+  return <Card className="deal" >
+    <div className="deal__sections">
+      <div className="deal__section-1">
+        <div className="avatar">
+          { avatar ? <img src={avatar} /> : <Avatar /> }
+        </div>
+      </div>
+      <div className="deal__section-2">
+        <a href="#" className="deal__title">{ title }</a>
+        <div className="mt-1">
+          <a href="#" className="deal__username">{username}</a>
+          <span className="deal__posted-at">{createdAt}</span>
+        </div>
+        <p className="deal__description">{description}</p>
+        <div>
+          <div className="deal__tags">
+            {tags.map(tag => {
+              return <span>{tag}</span>
+            })}
+          </div>
+          <span className="deal__expired-at">{expiredAt}</span>
+        </div>
+      </div>
+    </div>
+  </Card>
+}
 
-    </Card>
+export default function LatestDeals({ items }) {
+  return <div className="latest-deals mt-2">
+    <h3>Today</h3>
+    { items.map(item => {
+      return <Deal { ...item } />
+    }) }
   </div>
 }
--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,39 @@ import BrowseCategories from '../components/BrowseCategories';
 import LatestDeals from '../components/LatestDeals';
 import MainCarousel from '../components/MainCarousel';
 
+const latestDeals = [
+  {
+    title: 'Free 14 LinkedIn learning courses', 
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.", 
+    createdAt: '2020-09-01', 
+    expiredAt: '2020-10-01', 
+    username: 'john',
+    avatar: null,
+    tags: ['Education', 'Freebei'],
+    image: 'https://unsplash.com/photos/XzERvOnfH7M'
+  },
+  {
+    title: 'Free 14 LinkedIn learning courses', 
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.", 
+    createdAt: '2020-09-01', 
+    expiredAt: '2020-10-01', 
+    username: 'john',
+    avatar: null,
+    tags: ['Education', 'Freebei'],
+    image: 'https://unsplash.com/photos/XzERvOnfH7M'
+  },
+  {
+    title: 'Free 14 LinkedIn learning courses', 
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.", 
+    createdAt: '2020-09-01', 
+    expiredAt: '2020-10-01', 
+    username: 'john',
+    avatar: null,
+    tags: ['Education', 'Freebei'],
+    image: 'https://unsplash.com/photos/XzERvOnfH7M'
+  }
+]
+
 export default function Home() {
   return (
     <AppLayout>
@@ -12,7 +45,7 @@ export default function Home() {
           <div className="mr-2"><BrowseCategories /></div>
           <div className="flex-grow-1">
             <MainCarousel />
-            <LatestDeals />
+            <LatestDeals items={latestDeals} />
           </div>
         </div>
       </Container>
