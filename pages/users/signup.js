20cc349fbecce8d5bfd65be64df6cc139565a842
Hor <sokhor.gr@gmail.com>
2020-10-06 21:08:51 +1100

    completed created signup form

--- pages/users/signup.js
+++ pages/users/signup.js
@@ -1,11 +1,62 @@
 import AppLayout from '../../layouts/AppLayout';
-import { Container } from 'react-bootstrap';
+import { Container, Form, Col, Button } from 'react-bootstrap';
 
 export default function signup() {
   return (
     <AppLayout>
-      <Container>
-        <h2>Register</h2>
+      <Container className="signup">
+        <div>
+          <h2>Create new account</h2>
+        </div>
+        <div>
+          <Form>
+            <Form.Group>
+              <Col sm={10}>
+                <Form.Control type="user" placeholder="Username" />
+              </Col>
+            </Form.Group>
+
+            <Form.Group>
+              <Col sm={10}>
+                <Form.Control type="email" placeholder="Email" />
+              </Col>
+            </Form.Group>
+
+            <Form.Group>
+              <Col sm={10}>
+                <Form.Control type="password" placeholder="Password" />
+                <span className="signup__password-policy">
+                  Password must be at least 6 characters
+                </span>
+              </Col>
+            </Form.Group>
+
+            <Form.Group>
+              <Col sm={10}>
+                <Form.Control
+                  type="re-password"
+                  placeholder="Re-enter Password"
+                />
+              </Col>
+            </Form.Group>
+
+            <Form.Group>
+              <Col className="btn-block__signup">
+                <Button variant="primary" size="lg" type="submit" block>
+                  Create your account
+                </Button>
+                <span className="signup__user-policy">
+                  By creating an account, you agree that you have read and
+                  accepted our Conditions of Use and Private Notice.
+                </span>
+              </Col>
+            </Form.Group>
+            <hr />
+            <p className="signup__already-have-account">
+              Already have an account?<a href="/users/signin"> Sign in</a>
+            </p>
+          </Form>
+        </div>
       </Container>
     </AppLayout>
   );
