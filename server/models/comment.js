ca89caabdbaf0c2180cf00d11474db4ffc933efb
Hor <sokhor.gr@gmail.com>
2021-02-06 19:41:43 +1100

    Fix post to deal

--- server/models/comment.js
+++ server/models/comment.js
@@ -3,14 +3,14 @@ const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Comment extends Model {
     static associate(models) {
-      models.Comment.belongsTo(models.Post, { as: 'post' });
+      models.Comment.belongsTo(models.Deal, { as: 'deal' });
       models.Comment.belongsTo(models.User, { as: 'user' });
     }
   }
 
   Comment.init({
     userId: DataTypes.UUID,
-    postId: DataTypes.BIGINT,
+    dealId: DataTypes.BIGINT,
     body: DataTypes.TEXT,
   }, {
     sequelize,
--- server/models/deal.js
+++ server/models/deal.js
@@ -3,12 +3,12 @@ const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Deal extends Model {
     static associate(models) {
-      models.Post.hasMany(models.Comment, { as: 'comments' });
-      models.Post.belongsTo(models.User, { as: 'user' });
+      models.Deal.hasMany(models.Comment, { as: 'comments' });
+      models.Deal.belongsTo(models.User, { as: 'user' });
     }
   }
 
-  Post.init({
+  Deal.init({
     userId: DataTypes.UUID,
     title: DataTypes.STRING,
     body: DataTypes.TEXT,
