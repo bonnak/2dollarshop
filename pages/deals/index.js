5caaec2d80278c36b625aefac6256a4838d9c085
Hor <sokhor.gr@gmail.com>
2021-02-06 19:42:29 +1100

    Move to deals page

---
+++ pages/deals/index.js
@@ -0,0 +1,14 @@
+import { Container } from 'react-bootstrap';
+import AppLayout from '../../layouts/AppLayout';
+
+export default function PostPages () {
+  return (
+    <AppLayout>
+      <Container>
+        <div>
+          <h2>Deals Page</h2>
+        </div>
+      </Container>
+    </AppLayout>
+  );
+}
