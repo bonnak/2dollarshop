11521b43129fbf9c01761cc14255530619949fe0
Hor <sokhor.gr@gmail.com>
2021-03-20 20:14:52 +1100

    User manage his deals in office

--- components/PageHeader.js
+++ components/PageHeader.js
@@ -1,7 +1,5 @@
 import React, { useContext } from 'react';
-import {
-  Navbar, Nav, Container, NavDropdown,
-} from 'react-bootstrap';
+import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
 import GlobalSearch from './GlobalSearch';
 import { Context as AuthContext } from '../contexts/AuthContext';
 
@@ -22,23 +20,21 @@ export default function PageHeader() {
             <GlobalSearch />
           </div>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="nav-bar-right">
+          <Navbar.Collapse>
             <Nav>
-              <NavDropdown title="Deals" id="collasible-nav-dropdown dark">
-                <NavDropdown.Item href="/products/new-deals">
+              <NavDropdown title="Deals">
+                <NavDropdown.Item href="/deals/new-deals">
                   New Deals
                 </NavDropdown.Item>
-                <NavDropdown.Item href="/products/freebie">
+                <NavDropdown.Item href="/deals/freebie">
                   Freebie
                 </NavDropdown.Item>
-                <NavDropdown.Item href="/products/stores">
-                  Stores
-                </NavDropdown.Item>
-                <NavDropdown.Item href="/products/popular-deals">
+                <NavDropdown.Item href="/deals/stores">Stores</NavDropdown.Item>
+                <NavDropdown.Item href="/deals/popular-deals">
                   Popular Deals
                 </NavDropdown.Item>
               </NavDropdown>
-              { !auth.authenticated ? (
+              {!auth.authenticated ? (
                 <>
                   <Nav.Item>
                     <Nav.Link href="/users/signin">Sign-in</Nav.Link>
@@ -48,10 +44,15 @@ export default function PageHeader() {
                   </Nav.Item>
                 </>
               ) : (
-                <Nav.Item>
-                  <Nav.Link href="/users/profile">{auth.user.name}</Nav.Link>
-                </Nav.Item>
-              ) }
+                <NavDropdown title={auth.user.name}>
+                  <NavDropdown.Item href="/_office/deals">
+                    Deals
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="/_office/users/profile">
+                    Profile
+                  </NavDropdown.Item>
+                </NavDropdown>
+              )}
             </Nav>
           </Navbar.Collapse>
         </Navbar>
---
+++ pages/_office/deals/index.js
@@ -0,0 +1,3 @@
+export default function DealsPage() {
+  return <div>This is deals</div>;
+}
